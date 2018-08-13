f1968926792eb2638589a76e56d8e98c1faa2dbd
Karen C <chenyanhuan66@163.com>
2018-08-13 01:52:37 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,7 @@ class Patient
   end
 
   def new_appointment(doctor,date)
-    app =  Appointment.new(date, self, doctor)
+    app =  Appointment.new(self, date, doctor)
     @appointments << app
     app
   end
